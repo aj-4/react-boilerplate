9507e53ef1afe5c1c174fb6e9c68c0024c9bb466
Aaron Pietsch <apietsch4@gmail.com>
2018-03-28 09:17:00 -0700

    lite version

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import ExampleReducer from './reducer_example';
-
-const rootReducer = combineReducers({
-  example: ExampleReducer
-});
-
-export default rootReducer;
